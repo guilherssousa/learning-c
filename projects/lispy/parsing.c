72457755742b94c2f9ac21ba907d70add6a0ad46
Guilherme Sousa <guilherssousa@gmail.com>
2024-08-14 23:36:13 -0300

    TODO: Investigate why (/ ()) evlauates wihtout error

--- projects/lispy/parsing.c
+++ projects/lispy/parsing.c
@@ -186,59 +186,133 @@ void lval_println(lval *v) {
   putchar('\n');
 }
 
-// Check operator and run operation
-// lval *eval_op(lval *x, char *op, lval *y) {
-//   if (x->type == LVAL_ERR) {
-//     return x;
-//   }
-//   if (y->type == LVAL_ERR) {
-//     return y;
-//   }
-//
-//   if (strcmp(op, "+") == 0) {
-//     return lval_num(x->num + y->num);
-//   }
-//   if (strcmp(op, "-") == 0) {
-//     return lval_num(x->num - y->num);
-//   }
-//   if (strcmp(op, "*") == 0) {
-//     return lval_num(x->num * y->num);
-//   }
-//   if (strcmp(op, "/") == 0) {
-//     return y->num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x->num / y->num);
-//   }
-//   if (strcmp(op, "%") == 0) {
-//     return lval_num(x->num % y->num);
-//   }
-//   if (strcmp(op, "^") == 0) {
-//     return lval_num(pow(x->num, y->num));
-//   }
-//
-//   return lval_err(LERR_BAD_OP);
-// }
-//
-// lval *eval(mpc_ast_t *t) {
-//   // If node tag is a number, cast and return
-//   if (strstr(t->tag, "number")) {
-//     errno = 0;
-//     long x = strtol(t->contents, NULL, 10);
-//     return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
-//   }
-//
-//   // Ignore ( when expr is not number
-//   char *op = t->children[1]->contents;
-//   lval *x = eval(t->children[2]);
-//
-//   // Iterate remaining children and combine
-//   int i = 3;
-//
-//   while (strstr(t->children[i]->tag, "expr")) {
-//     x = eval_op(x, op, eval(t->children[i]));
-//     i++;
-//   }
-//
-//   return x;
-// }
+lval *lval_pop(lval *v, int i) {
+  /* Find item at "i" */
+  lval *x = v->cell[i];
+
+  /* Shift memory after the item at "i" over the top */
+  memmove(&v->cell[i], &v->cell[i + 1], sizeof(lval *) * (v->count - i - 1));
+
+  /* Decrease the count of items in the list */
+  v->count--;
+
+  /* Reallocate the memory used */
+  v->cell = realloc(v->cell, sizeof(lval *) * v->count);
+  return x;
+}
+
+lval *lval_take(lval *v, int i) {
+  lval *x = lval_pop(v, i);
+  lval_del(v);
+  return x;
+}
+
+lval *builtin_op(lval *a, char *op) {
+  /* Ensure all arguments are numbers */
+  for (int i = 0; i < a->count; i++) {
+    if (a->cell[i]->type != LVAL_NUM) {
+      lval_del(a);
+      return lval_err("Cannot operate on non-Number!");
+    }
+  }
+
+  /* Pop first element */
+  lval *x = lval_pop(a, 0);
+
+  /*
+    If no arguments and op is sub, perform unary negation
+    This handles special values like (- 5) that should
+    evaluate to -5.
+  */
+  if ((strcmp(op, "-") == 0) && a->count == 0) {
+    x->num = -x->num;
+  }
+
+  /* While there are still elements remaining */
+  while (a->count > 0) {
+    /* Pop next element */
+    lval *y = lval_pop(a, 0);
+
+    if (strcmp(op, "+") == 0) {
+      x->num += y->num;
+    }
+    if (strcmp(op, "-") == 0) {
+      x->num -= y->num;
+    }
+    if (strcmp(op, "*") == 0) {
+      x->num *= y->num;
+    }
+    if (strcmp(op, "/") == 0) {
+      if (y->num == 0) {
+        lval_del(x);
+        lval_del(y);
+        x = lval_err("Division by Zero!");
+        break;
+      }
+      x->num /= y->num;
+    }
+    if (strcmp(op, "%") == 0) {
+      x->num %= y->num;
+    }
+    if (strcmp(op, "^") == 0) {
+      x->num ^= y->num;
+    }
+
+    lval_del(y);
+  }
+
+  lval_del(a);
+
+  return x;
+}
+
+lval *lval_eval(lval *v);
+
+lval *lval_eval_sexpr(lval *v) {
+  /* Evaluate children */
+  for (int i = 0; i < v->count; i++) {
+    v->cell[i] = lval_eval(v->cell[i]);
+  }
+
+  /* Error checking */
+  for (int i = 0; i < v->count; i++) {
+    if (v->cell[i]->type == LVAL_ERR) {
+      return lval_take(v, i);
+    }
+  }
+
+  /* Empty Expression */
+  if (v->count == 0)
+    return v;
+
+  /* Single expression */
+  if (v->count == 1)
+    return lval_take(v, 0);
+
+  /* Ensure first element is a symbol */
+  lval *f = lval_pop(v, 0);
+  if (f->type != LVAL_SYM) {
+    lval_del(f);
+    lval_del(v);
+    return lval_err("S-Expression does not start with Symbol!");
+  }
+
+  /* Call builtin with operator */
+  lval *result = builtin_op(v, f->sym);
+  lval_del(f);
+
+  return result;
+}
+
+lval *lval_eval(lval *v) {
+  /* Evaluate S-Expressions */
+  if (v->type == LVAL_SEXPR) {
+    return lval_eval_sexpr(v);
+  }
+
+  /* Other types remain the same */
+  return v;
+}
 
 int number_of_nodes(mpc_ast_t *t) {
   if (t->children_num == 0) {
