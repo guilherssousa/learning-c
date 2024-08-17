5fe6f0218c563efb4b5cd6408169164239843295
Guilherme Sousa <guilherssousa@gmail.com>
2024-08-16 21:01:01 -0300

    Fix variable bind bug

--- projects/lispy/lval.c
+++ projects/lispy/lval.c
@@ -254,6 +254,9 @@ lval *lval_call(lenv *e, lval *f, lval *a) {
     /* Pop the next argument from the list */
     lval *val = lval_pop(a, 0);
 
+    /* Bind a copy into the function's environment */
+    lenv_put(f->env, sym, val);
+
     /* Delete symbol and value */
     lval_del(sym);
     lval_del(val);
